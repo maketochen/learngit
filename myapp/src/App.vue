7bf6581b4483b553797ae28b05cd0e151836b1d2
cheneyzhe <1120848233@qq.com>
2022-07-14 22:01:35 +0800

    vue demo

---
+++ myapp/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div id="app">
+    {{ msg }}
+    <div>
+      <input type="text" v-model="info" />
+      <button @click="handleClick">添加</button>
+    </div>
+    <ul>
+      <todo-item v-for="item in list" :key="item">
+        <template v-slot:item="itemProps">
+          <span :style="{ fontSize: '20px', color: itemProps.checked ? 'red' : 'blue' }">{{ item }}</span>
+        </template>
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem'
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "hello msg",
+      info: "",
+      list: [],
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.info);
+      this.list.push(this.info);
+      this.info = "";
+    },
+  },
+  components: {
+    TodoItem,
+  },
+};
+</script>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+</style>
---
+++ myapp/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="item">
+    <!-- {{ item }} -->
+    <input type="checkbox" v-model="checked">
+    <slot name="item" v-bind="{checked}"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+  data() {
+    return {
+      checked: false
+    };
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  color: red;
+}
+</style>
